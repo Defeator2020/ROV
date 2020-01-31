498d4c515d796fe12b4ad10a53855302e43fa7e2
Architect <ethanhgraham0@gmail.com>
2020-01-31 09:12:09 -0800

    Update ROV_Board.ino

--- ROV_Board/ROV_Board.ino
+++ ROV_Board/ROV_Board.ino
@@ -1,12 +1,9 @@
 #include <Servo.h>
 
-const int ledPin = 13; // The pin that the LED is attached to
-
 // Create servo objects to control everything (servos + ESCs)
 Servo thrusterL;
 Servo thrusterR;
 Servo thrusterV;
-Servo panCamera;
 Servo tiltCamera;
 
 // Create variables to store the command data
@@ -15,13 +12,18 @@ int pos2 = 90;
 int pos3 = 90;
 int peripheralControl = 0;
 
+// Create variables to manage the LED conditions and logic
+const int ledPin = 4;    // LED connected to digital pin 9
+float ledValue = 180; // LED is inverted from usual
+const int ledMax = 255; // 0 - 255 (WRITE COMMAND TO USE SERIAL TO FIND ABSOLUTE MAX / MIN VALUES FOR THIS)
+const int ledMin = 0; // 0 - 255 (")
+
 void setup() {
   // Attach the thrusters and servos to their respective pins
   thrusterL.attach(5);
   thrusterR.attach(6);
   thrusterV.attach(7);
-  panCamera.attach(9);
-  tiltCamera.attach(10);
+  tiltCamera.attach(8);
 
   // Perform ESC handshake(s) - CURRENTLY ONLY LEFT THRUSTER (PIN 5)
   thrusterL.write(140);
@@ -30,7 +32,6 @@ void setup() {
   delay(4000);
   
   Serial.begin(9600); // Initialize serial communication
-  pinMode(ledPin, OUTPUT); // Initialize the LED pin as an output
 }
 
 void loop() {
@@ -53,13 +54,23 @@ void loop() {
         thrusterL.write(pos1);
         thrusterR.write(pos2);
         thrusterV.write(pos3);
+        
       } else if (peripheralControl == 1) {
-        panCamera.write(pos1);
         tiltCamera.write(pos2);
+        
       } else if (peripheralControl == 2) {
-    
-      } else {
-    
+        pos1 = constrain(pos1,-1,1); // WILL CHANGING THIS VALUE (pos1) HERE MESS ANYTHING UP? DO I NEED TO MAKE A NEW VARIABLE JUST FOR THIS CALCULATION?
+        ledValue += pos1 * -1; // -1 is needed because the PWM controller is inverted (255(ish) = min, 0(ish) = max)
+        ledValue = constrain(ledValue, ledMin, ledMax);
+
+        analogWrite(ledPin, ledValue);
+        delay(10);
+        
+      } else { // TEMPORARY ERROR STATE - NO THRUSTERS, CAMERA CENTERED
+        thrusterL.write(90); // VERIFY THAT THIS REALLY IS 0% (NEUTRAL) THROTTLE
+        thrusterR.write(90);
+        thrusterV.write(90);
+        tiltCamera.write(90);
       }
     }
   }
