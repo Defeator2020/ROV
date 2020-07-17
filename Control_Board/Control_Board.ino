c37d2bdf8723cc7695abe5db1afbb845ec3e7b29
Defeator2020 <ethanhgraham0@gmail.com>
2020-07-17 13:22:15 -0700

    Changed thruster values for vertical thruster so as to combat their being outside the viable range
    This should be a constraint at some point, too

--- Control_Board/Control_Board.ino
+++ Control_Board/Control_Board.ino
@@ -14,8 +14,10 @@ float pivotYlimit = 23; // The threshold where pivoting begins, in units of the 
 
 int thrusterNeutral = 90; // Neutral (default) value for the thrusters (primarily used for error codes) 
 float ratePercent = 1; // Adjusts speed of motors based on rotary selector position (0 - 1)
-int thrusterMinValue = 0; // Sets minimum throttle level for ESC - REMEMBER TO SET PRIOR TO TESTING (0 - 180)
-int thrusterMaxValue = 180; // Sets maximum throttle level for ESC - REMEMBER TO SET PRIOR TO TESTING (0 - 180)
+int thrusterMinValue = 0; // Sets minimum throttle level for ESC (0 - 180)
+int thrusterMaxValue = 180; // Sets maximum throttle level for ESC (0 - 180)
+int verticalThrusterMin = 7; // Sets minimum throttle level for vertical ESC (0 - 180)
+int verticalThrusterMax = 179; // Sets maximum throttle level for vertical ESC (0 - 180)
 
 int peripheralControl = 0; // Tells the ROV's Arduino if the values are for the thrusters or for an accessory (0=thrusters, 1=camera, 2=misc)
 
@@ -189,9 +191,9 @@ void calculateThrusters() {
 
   // Calculate 3rd motor value (pos3)
   if (cbut == 1 && zbut == 0) {
-    pos3 = 89; // Not full 90 to try and resolve the no down thruster on full rate setting
+    pos3 = verticalThrusterMax - 90; // Not full 90 to try and resolve the no down thruster on full rate setting
   } else if (cbut == 0 && zbut == 1) {
-    pos3 = -89; // Not full 90 to try and resolve the no down thruster on full rate setting
+    pos3 = verticalThrusterMin - 90; // Not full 90 to try and resolve the no down thruster on full rate setting
   } else {
     pos3 = 0;
   }
